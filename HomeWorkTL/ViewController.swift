fdaa695c9f63482cffbfd902ccb7e2ad85021208
XXNNDDRR <xndrgo@gmail.com>
2023-11-04 13:13:13 +0300

    Develop TL

--- HomeWorkTL/ViewController.swift
+++ HomeWorkTL/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var button: UIButton!
+    
+    private var views: [UIView] = []
+    private var currentViewIndex = 0
+    private var isFirstClick = true
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        views = [
+            redView,
+            yellowView,
+            greenView
+        ]
+        
+        for view in views {
+            view.alpha = 0.1
+            view.layer.cornerRadius = 75
+        }
+        
+        button.layer.cornerRadius = 20
+        
+        
+        
     }
-
-
+    
+    @IBAction func clickTurnOn(_ sender: UIButton) {
+        
+        if isFirstClick {
+            redView.alpha = 1.0
+            isFirstClick = false
+            button.setTitle("NEXT", for: .normal)
+        } else {
+            let previousView = views[currentViewIndex]
+            previousView.alpha = 0.1
+            
+            currentViewIndex = (
+                currentViewIndex + 1
+            ) % views.count
+            let currentView = views[currentViewIndex]
+            currentView.alpha = 1.0
+        }
+        
+    }
+    
 }
 
